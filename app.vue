833e1b704eb40705784df1a7a64514889a5c99d8
1weidong <649149488@qq.com>
2018-01-18 13:13:12 +0800

    daily

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="daily">
+        <div class="daily-meun">
+            <div class="daily-menu-item">每日推荐</div>
+            <div class="daily-menu-item">主题日报</div>
+        </div>
+        <div class="daily-list">
+            <item></item>
+        </div>
+        <daily-ariticle></daily-ariticle>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+    
+</style>
